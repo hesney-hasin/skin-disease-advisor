a4f4314ea5c0f4c53748ec0d165f6c6decfb6114
Hasnay Hasin <hesneyhasinmaliha@gmail.com>
2026-04-11 01:01:47 +0600

    Delete upload.py

--- upload.py
+++
@@ -1,8 +0,0 @@
-from huggingface_hub import HfApi
-
-api = HfApi()
-api.upload_folder(
-    folder_path="D:/skin-disease-advisor/skin-disease-advisor",
-    repo_id="hesneyhasin/skin-disease-advisor",
-    repo_type="space"
-)
